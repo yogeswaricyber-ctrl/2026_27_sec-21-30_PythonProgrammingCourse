8185b93811896555763e8ea626e50b024103190d
yogeswaricyber-ctrl <yogeswaricyber@gmail.com>
2026-09-18 14:16:31 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -2,3 +2,8 @@
 a=2
 b=5
 print("sum of a and b is:",a+b)
+print("sum of a and b is:",a-b)
+print("sum of a and b is:",a*b)
+print("sum of a and b is:",a**b)
+print("sum of a and b is:",a/b)
+print("sum of a and b is:",a//b)
